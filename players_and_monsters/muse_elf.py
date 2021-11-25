34949a36c697eca963cf1b67a167d5f45aa15943
Petko Slavov <petkoslavov89@gmail.com>
2021-11-26 00:37:23 +0200

    Add files via upload

---
+++ players_and_monsters/muse_elf.py
@@ -0,0 +1,7 @@
+from players_and_monsters.elf import Elf
+
+
+class MuseElf(Elf):
+    def __init__(self, username, level):
+        super().__init__(username, level)
+
---
+++ players_and_monsters/soul_master.py
@@ -0,0 +1,7 @@
+from players_and_monsters.dark_wizard import DarkWizard
+
+
+class SoulMaster(DarkWizard):
+    def __init__(self, username, level):
+        super().__init__(username, level)
+
