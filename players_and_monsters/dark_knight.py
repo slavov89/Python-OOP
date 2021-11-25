4a29d0fb6423d15de2671fa557f07d8a38ddde54
Petko Slavov <petkoslavov89@gmail.com>
2021-11-26 00:42:29 +0200

    Add files via upload

---
+++ players_and_monsters/dark_knight.py
@@ -0,0 +1,9 @@
+from players_and_monsters.knight import Knight
+
+
+class DarkKnight(Knight):
+    def __init__(self, username, level):
+        super().__init__(username, level)
+
+    def __str__(self):
+        return f"{self.username} of type {DarkKnight.__name__} has level {self.level}"
---
+++ players_and_monsters/elf.py
@@ -0,0 +1,7 @@
+from players_and_monsters.hero import Hero
+
+
+class Elf(Hero):
+    def __init__(self, username, level):
+        super().__init__(username, level)
+
