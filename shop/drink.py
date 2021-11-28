a10251fd939931a2c5880e85626bbdcd25f474cf
Petko Slavov <petkoslavov89@gmail.com>
2021-11-28 15:10:45 +0200

    Add files via upload

---
+++ shop/drink.py
@@ -0,0 +1,7 @@
+from shop.product import Product
+
+
+class Drink(Product):
+    def __init__(self, name):
+        super().__init__(name, 10)
+
---
+++ shop/product_repository.py
@@ -0,0 +1,26 @@
+from shop.product import Product
+
+
+class ProductRepository:
+    def __init__(self):
+        self.products = []
+
+    def add(self, product: Product):
+        self.products.append(product)
+
+    def find(self, product_name):
+        for product in self.products:
+            if product.name == product_name:
+                return product
+
+    def remove(self, product_name):
+        for product in self.products:
+            if product.name == product_name:
+                self.products.remove(product)
+
+    def __repr__(self):
+        info = ""
+        for product in self.products:
+            info += f"{product.name}: {product.quantity}\n"
+
+        return info.strip()
